64c89b0dcd738ba877872b999565c4d1eefeba51
Mustafa Burak Orhan <32410197+borhan23@users.noreply.github.com>
2022-12-30 00:19:16 +0300

    Add files via upload
    servlet example with print date

---
+++ SelamWeb/src/main/java/x/y/SelamServlet.java
@@ -0,0 +1,33 @@
+package x.y;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SelamServlet
+ */
+@WebServlet({ "/SelamServlet", "/selam" })
+public class SelamServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("time: ").print(new Date());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
